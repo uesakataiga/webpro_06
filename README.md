0458a979db660e112349167bfd45ce716bdf7ac8
uesakataiga <basketballs0831@gmail.com>
2024-11-12 10:31:05 +0900

    レポートを追加

--- README.md
+++ README.md
@@ -38,3 +38,44 @@ win --> end1
 if --> |no| lose
 lose --> end1
 ```
+
+
+## レポート
+
+#### (1)
+・起動方法は```node app5.js```とターミナルで起動させ，Webブラウザでlocalhost:8080/public/janken.htmlにアクセスする．janken.htmlの部分はアクセスしたいhtmlの名前を入力する．
+
+・編集したファイルは
+```git add .```
+```git commit -am 'コメント'```
+```git push```
+と入力することで，Github上に編集したファイルと編集箇所をコメントしたものをアップロードすることができる．
+
+#### (2)
+1. /janken
+機能はコンピュータとジャンケンすることである．
+```let hand = req.query.hand;```
+```let win = Number( req.query.win );```
+```let total = Number( req.query.total );```
+では，入力したジャンケンの手を読み取るhandと勝利数を読み取るwin，試合数を読み取るtotalの値を取得している．
+```const num = Math.floor( Math.random() * 3 + 1 );```
+では，1から3までの整数をランダムに決めている．そして，numの値が1ならグーを，2ならチョキ，3ならパーをコンピュータが出すようにしている．また，if文でWebブラウザでユーザーが入力したジャンケンの手とコンピュータが出した手によって勝敗の判定をしていて，totalを1増やし，勝った場合のみwinを1増やす．
+
+2. /attimuitehoi
+機能はあっち向いてホイをユーザー側が顔，コンピュータ側が指を動かして行うものである．
+```let face = req.query.face;```
+```let win = Number( req.query.win );```
+```let total = Number( req.query.total );```
+では，それぞれ入力した上下左右を読み取るfaceと勝利数を読み取るwin，試合数を読み取るtotalを取得している．
+```const num = Math.floor( Math.random() * 4 + 1 );```
+では，1から4までの整数をランダムに決めている．そして，numの値が1なら上を，2なら下，3なら右，4なら左をコンピュータが指すようにしている．また，if文でWebブラウザでユーザーが入力した顔の向きとコンピュータが指した方向によって勝敗の判定をしていて，totalを1増やし，勝った場合のみwinを1増やす．
+
+3. /tyouhann
+機能は丁半をユーザーが丁か半か決め，コンピュータが出目を出力して行うものである．
+```let card = req.query.card;```
+```let win = Number( req.query.win );```
+```let lose = Number( req.query.lose );```
+では，それぞれ入力した丁か半を読み取るcardと勝利数を読み取るwin，敗北数を読み取るloseを取得している．
+```const num = Math.floor( Math.random() * 6 + 1 );```
+```const num1 = Math.floor( Math.random() * 6 + 1 );```
+では，それぞれ1から6までの整数をランダムに決めている．numとnum1の値で出目を定めている．そして，if文でWebブラウザでユーザーが入力した丁か半とコンピュータが出力した出目によって勝敗の判定をしていて，勝った場合のみwinを1増やし，負けた場合のみloseを1増やす．
--- app5.js
+++ app5.js
@@ -91,4 +91,521 @@ app.get("/janken", (req, res) => {
   res.render( 'janken', display );
 });
 
+app.get("/attimuitehoi", (req, res) => {
+  let face = req.query.face;
+  let win = Number( req.query.win );
+  let total = Number( req.query.total );
+  console.log( {face, win, total});
+  const num = Math.floor( Math.random() * 4 + 1 );
+  let judgement = '';
+  let cpu = '';
+  if( num==1 ) cpu = '上';
+  else if( num==2 ) cpu = '下';
+  else if ( num==3 ) cpu = '右';
+  else if ( num==4 ) cpu = '左';
+  // ここに勝敗の判定を入れる
+  if(isNaN(win)) win = 0;
+  if(isNaN(total)) total = 0;
+  
+  if(num == 1) {
+    if(face == '上') {
+      judgement = '負け';
+      total+=1;
+    } else if (face == '下') {
+      judgement = '勝ち';
+      total+=1;
+      win+=1;
+    } else if (face == '右') {
+      judgement = '勝ち';
+      total+=1;
+      win+=1;
+    } else if (face == '左') {
+      judgement = '勝ち';
+      total+=1;
+      win+=1;
+    }
+  }
+  else if(num == 2) {
+    if(face == '上') {
+      judgement = '勝ち';
+      win+=1;
+      total+=1;
+    } else if (face == '下') {
+      judgement ='負け';
+      total+=1;
+    } else if (face == '右') {
+      judgement = '勝ち';
+      win+=1;
+      total+=1;
+    } else if (face == '左') {
+      judgement = '勝ち';
+      win+=1;
+      total+=1;
+    }
+  }
+  else if(num == 3) {
+    if(face == '上') {
+      judgement = '勝ち';
+      win+=1;
+      total+=1;
+    } else if (face == '下') {
+      judgement ='勝ち';
+      win+=1;
+      total+=1;
+    } else if(face == '右') {
+      judgement = '負け';
+      total+=1;
+    } else if (face == '左') {
+      judgement = '勝ち';
+      win+=1;
+      total+=1;
+    }
+  }
+  else if(num == 4) {
+    if(face == '上') {
+      judgement = '勝ち';
+      win+=1;
+      total+=1;
+    } else if (face == '下') {
+      judgement ='勝ち';
+      win+=1;
+      total+=1;
+    } else if(face == '右') {
+      judgement = '勝ち';
+      win+=1;
+      total+=1;
+    } else if (face == '左') {
+      judgement = '負け';
+      total+=1;
+    }
+  }
+  const display = {
+    your: face,
+    cpu: cpu,
+    judgement: judgement,
+    win: win,
+    total: total
+  }
+  res.render( 'attimuitehoi', display );
+});
+
+app.get("/tyouhann", (req, res) => {
+  let card = req.query.card;
+  let win = Number( req.query.win );
+  let lose = Number( req.query.lose );
+  console.log( {card, win, lose});
+  const num = Math.floor( Math.random() * 6 + 1 );
+  const num1 = Math.floor( Math.random() * 6 + 1 );
+  let judgement = '';
+  let cpu = '';
+  if( num==1 && num1==1 ) cpu = ' 1,1 ';
+  else if( num==1 && num1==2 ) cpu = ' 1,2 ';
+  else if( num==2 && num1==1 ) cpu = ' 2,1 ';
+  else if( num==1 && num1==3 ) cpu = ' 1,3 ';
+  else if( num==3 && num1==1 ) cpu = ' 3,1 ';
+  else if( num==1 && num1==4 ) cpu = ' 1,4 ';
+  else if( num==4 && num1==1 ) cpu = ' 4,1 ';
+  else if( num==1 && num1==5 ) cpu = ' 1,5 ';
+  else if( num==5 && num1==1 ) cpu = ' 5,1 ';
+  else if( num==1 && num1==6 ) cpu = ' 1,6 ';
+  else if( num==6 && num1==1 ) cpu = ' 6,1 ';
+  else if( num==2 && num1==2 ) cpu = ' 2,2 ';
+  else if( num==2 && num1==3 ) cpu = ' 2,3 ';
+  else if( num==3 && num1==2 ) cpu = ' 3,2 ';
+  else if( num==2 && num1==4 ) cpu = ' 2,4 ';
+  else if( num==4 && num1==2 ) cpu = ' 4,2 ';
+  else if( num==2 && num1==5 ) cpu = ' 2,5 ';
+  else if( num==5 && num1==2 ) cpu = ' 5,2 ';
+  else if( num==2 && num1==6 ) cpu = ' 2,6 ';
+  else if( num==6 && num1==2 ) cpu = ' 6,2 ';
+  else if( num==3 && num1==3 ) cpu = ' 3,3 ';
+  else if( num==3 && num1==4 ) cpu = ' 3,4 ';
+  else if( num==4 && num1==3 ) cpu = ' 4,3 ';
+  else if( num==3 && num1==5 ) cpu = ' 3,5 ';
+  else if( num==5 && num1==3 ) cpu = ' 5,3 ';
+  else if( num==3 && num1==6 ) cpu = ' 3,6 ';
+  else if( num==6 && num1==3 ) cpu = ' 6,3 ';
+  else if( num==4 && num1==4 ) cpu = ' 4,4 ';
+  else if( num==4 && num1==5 ) cpu = ' 4,5 ';
+  else if( num==5 && num1==4 ) cpu = ' 5,4 ';
+  else if( num==4 && num1==6 ) cpu = ' 4,6 ';
+  else if( num==6 && num1==4 ) cpu = ' 6,4 ';
+  else if( num==5 && num1==5 ) cpu = ' 5,5 ';
+  else if( num==5 && num1==6 ) cpu = ' 5,6 ';
+  else if( num==6 && num1==5 ) cpu = ' 6,5 ';
+  else if( num==6 && num1==6 ) cpu = ' 6,6 ';
+  // ここに勝敗の判定を入れる
+  if(isNaN(win)) win = 0;
+  if(isNaN(lose)) lose = 0;
+  
+  if(num == 1 && num1 == 1) {
+    if(card == '丁') {
+      judgement = 'ピンゾロの丁';
+      win+=1;
+    }
+    else if(card == '半'){
+      judgement = 'ピンゾロの丁';
+      lose+=1;
+    }
+  }
+  if(num == 1 && num1 == 2) {
+    if(card == '丁') {
+      judgement = 'イチニの半';
+      lose+=1;
+    }
+    else if(card == '半'){
+      judgement = 'イチニの半';
+      win+=1;
+    }
+  }
+  if(num == 2 && num1 == 1) {
+    if(card == '丁') {
+      judgement = 'イチニの半';
+      lose+=1;
+    }
+    else if(card == '半'){
+      judgement = 'イチニの半';
+      win+=1;
+    }
+  }
+  if(num == 1 && num1 == 3) {
+    if(card == '丁') {
+      judgement = 'サンミチの丁';
+      win+=1;
+    }
+    else if(card == '半'){
+      judgement = 'サンミチの丁';
+      lose+=1;
+    }
+  }
+  if(num == 3 && num1 == 1) {
+    if(card == '丁') {
+      judgement = 'サンミチの丁';
+      win+=1;
+    }
+    else if(card == '半'){
+      judgement = 'サンミチの丁';
+      lose+=1;
+    }
+  }
+  if(num == 1 && num1 == 4) {
+    if(card == '丁') {
+      judgement = 'ヨイチの半';
+      lose+=1;
+    }
+    else if(card == '半'){
+      judgement = 'ヨイチの半';
+      win+=1;
+    }
+  }
+  if(num == 4 && num1 == 1) {
+    if(card == '丁') {
+      judgement = 'ヨイチの半';
+      lose+=1;
+    }
+    else if(card == '半'){
+      judgement = 'ヨイチの半';
+      win+=1;
+    }
+  }
+  if(num == 1 && num1 == 5) {
+    if(card == '丁') {
+      judgement = 'グイチの丁';
+      win+=1;
+    }
+    else if(card == '半'){
+      judgement = 'グイチの丁';
+      lose+=1;
+    }
+  }
+  if(num == 5 && num1 == 1) {
+    if(card == '丁') {
+      judgement = 'グイチの丁';
+      win+=1;
+    }
+    else if(card == '半'){
+      judgement = 'グイチの丁';
+      lose+=1;
+    }
+  }
+  if(num == 1 && num1 == 6) {
+    if(card == '丁') {
+      judgement = 'イチロクの半';
+      lose+=1;
+    }
+    else if(card == '半'){
+      judgement = 'イチロクの半';
+      win+=1;
+    }
+  }
+  if(num == 6 && num1 == 1) {
+    if(card == '丁') {
+      judgement = 'イチロクの半';
+      lose+=1;
+    }
+    else if(card == '半'){
+      judgement = 'イチロクの半';
+      win+=1;
+    }
+  }
+  if(num == 2 && num1 == 2) {
+    if(card == '丁') {
+      judgement = '二ゾロの丁';
+      win+=1;
+    }
+    else if(card == '半'){
+      judgement = '二ゾロの丁';
+      lose+=1;
+    }
+  }
+  if(num == 2 && num1 == 3) {
+    if(card == '丁') {
+      judgement = 'サニの半';
+      lose+=1;
+    }
+    else if(card == '半'){
+      judgement = 'サニの半';
+      win+=1;
+    }
+  }
+  if(num == 3 && num1 == 2) {
+    if(card == '丁') {
+      judgement = 'サニの半';
+      lose+=1;
+    }
+    else if(card == '半'){
+      judgement = 'サニの半';
+      win+=1;
+    }
+  }
+  if(num == 2 && num1 == 4) {
+    if(card == '丁') {
+      judgement = 'シニの丁';
+      win+=1;
+    }
+    else if(card == '半'){
+      judgement = 'シニの丁';
+      lose+=1;
+    }
+  }
+  if(num == 4 && num1 == 2) {
+    if(card == '丁') {
+      judgement = 'シニの丁';
+      win+=1;
+    }
+    else if(card == '半'){
+      judgement = 'シニの丁';
+      lose+=1;
+    }
+  }
+  if(num == 2 && num1 == 5) {
+    if(card == '丁') {
+      judgement = 'グニの半';
+      lose+=1;
+    }
+    else if(card == '半'){
+      judgement = 'グニの半';
+      win+=1;
+    }
+  }
+  if(num == 5 && num1 == 2) {
+    if(card == '丁') {
+      judgement = 'グニの半';
+      lose+=1;
+    }
+    else if(card == '半'){
+      judgement = 'グニの半';
+      win+=1;
+    }
+  }
+  if(num == 2 && num1 == 6) {
+    if(card == '丁') {
+      judgement = 'ニロクの丁';
+      win+=1;
+    }
+    else if(card == '半'){
+      judgement = 'ニロクの丁';
+      lose+=1;
+    }
+  }
+  if(num == 6 && num1 == 2) {
+    if(card == '丁') {
+      judgement = 'ニロクの丁';
+      win+=1;
+    }
+    else if(card == '半'){
+      judgement = 'ニロクの丁';
+      lose+=1;
+    }
+  }
+  if(num == 3 && num1 == 3) {
+    if(card == '丁') {
+      judgement = 'サンゾロの丁';
+      win+=1;
+    }
+    else if(card == '半'){
+      judgement = 'サンゾロの丁';
+      lose+=1;
+    }
+  }
+  if(num == 3 && num1 == 4) {
+    if(card == '丁') {
+      judgement = 'シソウの半';
+      lose+=1;
+    }
+    else if(card == '半'){
+      judgement = 'シソウの半';
+      win+=1;
+    }
+  }
+  if(num == 4 && num1 == 3) {
+    if(card == '丁') {
+      judgement = 'シソウの半';
+      lose+=1;
+    }
+    else if(card == '半'){
+      judgement = 'シソウの半';
+      win+=1;
+    }
+  }
+  if(num == 3 && num1 == 5) {
+    if(card == '丁') {
+      judgement = 'グサンの丁';
+      win+=1;
+    }
+    else if(card == '半'){
+      judgement = 'グサンの丁';
+      lose+=1;
+    }
+  }
+  if(num == 5 && num1 == 3) {
+    if(card == '丁') {
+      judgement = 'グサンの丁';
+      win+=1;
+    }
+    else if(card == '半'){
+      judgement = 'グサンの丁';
+      lose+=1;
+    }
+  }
+  if(num == 3 && num1 == 6) {
+    if(card == '丁') {
+      judgement = 'サブロクの半';
+      lose+=1;
+    }
+    else if(card == '半'){
+      judgement = 'サブロクの半';
+      win+=1;
+    }
+  }
+  if(num == 6 && num1 == 3) {
+    if(card == '丁') {
+      judgement = 'サブロクの半';
+      lose+=1;
+    }
+    else if(card == '半'){
+      judgement = 'サブロクの半';
+      win+=1;
+    }
+  }
+  if(num == 4 && num1 == 4) {
+    if(card == '丁') {
+      judgement = 'シゾロの丁';
+      win+=1;
+    }
+    else if(card == '半'){
+      judgement = 'シゾロの丁';
+      lose+=1;
+    }
+  }
+  if(num == 4 && num1 == 5) {
+    if(card == '丁') {
+      judgement = 'グシの半';
+      lose+=1;
+    }
+    else if(card == '半'){
+      judgement = 'グシの半';
+      win+=1;
+    }
+  }
+  if(num == 5 && num1 == 4) {
+    if(card == '丁') {
+      judgement = 'グシの半';
+      lose+=1;
+    }
+    else if(card == '半'){
+      judgement = 'グシの半';
+      win+=1;
+    }
+  }
+  if(num == 4 && num1 == 6) {
+    if(card == '丁') {
+      judgement = 'シロクの丁';
+      win+=1;
+    }
+    else if(card == '半'){
+      judgement = 'シロクの丁';
+      lose+=1;
+    }
+  }
+  if(num == 6 && num1 == 4) {
+    if(card == '丁') {
+      judgement = 'シロクの丁';
+      win+=1;
+    }
+    else if(card == '半'){
+      judgement = 'シロクの丁';
+      lose+=1;
+    }
+  }
+  if(num == 5 && num1 == 5) {
+    if(card == '丁') {
+      judgement = 'ゴゾロの丁';
+      win+=1;
+    }
+    else if(card == '半'){
+      judgement = 'ゴゾロの丁';
+      lose+=1;
+    }
+  }
+  if(num == 5 && num1 == 6) {
+    if(card == '丁') {
+      judgement = 'ゴロクの半';
+      lose+=1;
+    }
+    else if(card == '半'){
+      judgement = 'ゴロクの半';
+      win+=1;
+    }
+  }
+  if(num == 6 && num1 == 5) {
+    if(card == '丁') {
+      judgement = 'ゴロクの半';
+      lose+=1;
+    }
+    else if(card == '半'){
+      judgement = 'ゴロクの半';
+      win+=1;
+    }
+  }
+  if(num == 6 && num1 == 6) {
+    if(card == '丁') {
+      judgement = 'ロクゾロの丁';
+      win+=1;
+    }
+    else if(card == '半'){
+      judgement = 'ロクゾロの丁';
+      lose+=1;
+    }
+  }
+  const display = {
+    your: card,
+    cpu: cpu,
+    judgement: judgement,
+    win: win,
+    lose: lose
+  }
+  res.render( 'tyouhann', display );
+});
+
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
